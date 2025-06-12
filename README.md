23d0a5dc9ae0d49023eb2602f58525d79c5137e8
hinhbx <buixuanhinhxtnd@gmail.com>
2025-06-13 00:24:15 +0700

    feat: config env

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### Các bước cần làm để chạy dự án NestJS
 
 #### 1. Cài đặt thư viện với câu lệnh: npm i
-#### 2. Chạy dự án với câu lệnh: npm run dev
 
+#### 2. Chạy dự án với câu lệnh: npm run dev
 
 =================
 
@@ -14,7 +14,7 @@ Website chính thức: https://hoidanit.com.vn/
 
 Youtube “Hỏi Dân IT” : https://www.youtube.com/@hoidanit
 
-Tiktok “Hỏi Dân IT” :  https://www.tiktok.com/@hoidanit
+Tiktok “Hỏi Dân IT” : https://www.tiktok.com/@hoidanit
 
 Fanpage “Hỏi Dân IT” : https://www.facebook.com/askITwithERIC/
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
