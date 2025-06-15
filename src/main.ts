a060edb9908381b165e5c3d2e8881acd244cde95
hinhbx <buixuanhinhxtnd@gmail.com>
2025-06-15 23:15:08 +0700

    feat: how to validate in nestjs?

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Email is not valid' })
+  @IsNotEmpty({ message: 'Email is not empty' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password is not empty' })
   password: string;
+
+  name: string;
+  phone: string;
+  age: number;
 }
